bdd929ee83fc37deb2fcd87d3cbca058ee801bad
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-11-24 16:51:13 -0300

    Up.

---
+++ PostgreSQL_desenvolva_com_ PL_pgSQL/estrura_de_fluxo.sql
@@ -0,0 +1,99 @@
+CREATE FUNCTION salario_ok(instrutor instrutor) RETURNS VARCHAR AS $$ 
+	BEGIN
+		-- se o salário do instrutor for maior do que 200, está ok. Senão, pode aumentar
+		IF instrutor.salario > 200 THEN
+			RETURN 'Salário está ok';
+		ELSE
+			RETURN 'Salário pode aumentar';
+		END IF;
+	END;
+$$ LANGUAGE plpgsql;
+
+SELECT nome, salario_ok(instrutor) FROM instrutor;
+
+CREATE FUNCTION salario_ok_v2(id_instrutor INTEGER) RETURNS VARCHAR AS $$ 
+	DECLARE
+		instrutor instrutor;
+	BEGIN
+		SELECT * FROM instrutor WHERE id = id_instrutor INTO instrutor;
+		-- se o salário do instrutor for maior do que 200, está ok. Senão, pode aumentar
+		IF instrutor.salario > 200 THEN
+			RETURN 'Salário está ok';
+		ELSE
+			RETURN 'Salário pode aumentar';
+		END IF;
+	END;
+$$ LANGUAGE plpgsql;
+
+SELECT nome, salario_ok_v2(instrutor.id) FROM instrutor;
+
+CREATE FUNCTION salario_ok_v3(id_instrutor INTEGER) RETURNS VARCHAR AS $$ 
+	DECLARE
+		instrutor instrutor;
+	BEGIN
+		SELECT * FROM instrutor WHERE id = id_instrutor INTO instrutor;
+		-- se o salário do instrutor for maior do que 300, está ok. Se for 300 reais, então pode aumentar. Caso contrário, o salário está defasado.
+		IF instrutor.salario > 300 THEN
+			RETURN 'Salário está ok';
+		ELSEIF instrutor.salario = 300 THEN
+			RETURN 'Salário pode aumentar';
+		ELSE
+			RETURN 'Salário está defasado';
+		END IF;
+	END;
+$$ LANGUAGE plpgsql;
+
+SELECT nome, salario_ok_v3(instrutor.id) FROM instrutor;
+
+CREATE OR REPLACE FUNCTION salario_ok_v4(id_instrutor INTEGER) RETURNS VARCHAR AS $$ 
+	DECLARE
+		instrutor instrutor;
+	BEGIN
+		SELECT * FROM instrutor WHERE id = id_instrutor INTO instrutor;
+		-- se o salário do instrutor for maior do que 300, está ok. Se for 300 reais, então pode aumentar. Caso contrário, o salário está defasado.
+		CASE instrutor.salario
+			WHEN 100 THEN
+				RETURN 'Salário muito baixo';
+			WHEN 200 THEN
+				RETURN 'Salário baixo';
+			WHEN 300 THEN
+				RETURN 'Salário ok';
+			ELSE
+				RETURN 'Salário ótimo';
+		END CASE;
+	END;
+$$ LANGUAGE plpgsql;
+
+SELECT nome, salario_ok_v4(instrutor.id) FROM instrutor;
+
+CREATE OR REPLACE FUNCTION tabuada(numero INTEGER) RETURNS SETOF VARCHAR AS $$
+	DECLARE 
+		multiplicador INTEGER DEFAULT 1;
+	BEGIN
+		-- multiplicador que começa com 1, e vai até < 10
+		-- numero * multiplicador
+		LOOP
+			RETURN NEXT numero || ' x ' || multiplicador || ' = ' || numero * multiplicador;			
+			multiplicador := multiplicador + 1;			
+			EXIT WHEN multiplicador = 10;
+		END LOOP;
+	END;
+$$ LANGUAGE plpgsql;
+
+SELECT tabuada(9);
+
+
+CREATE OR REPLACE FUNCTION tabuada_v2(numero INTEGER) RETURNS SETOF VARCHAR AS $$
+	DECLARE 
+		multiplicador INTEGER DEFAULT 1;
+	BEGIN
+		-- multiplicador que começa com 1, e vai até < 10
+		-- numero * multiplicador
+		WHILE multiplicador < 10 LOOP
+			RETURN NEXT numero || ' x ' || multiplicador || ' = ' || numero * multiplicador;			
+			multiplicador := multiplicador + 1;			
+		END LOOP;
+	END;
+$$ LANGUAGE plpgsql;
+
+SELECT tabuada_v2(9);
--- PostgreSQL_desenvolva_com_ PL_pgSQL/tipos_compostos.sql
+++ PostgreSQL_desenvolva_com_ PL_pgSQL/tipos_compostos.sql
@@ -12,11 +12,11 @@ $$ LANGUAGE SQL;
 
 SELECT nome, dobro_do_salario(instrutor.*) AS desejo FROM instrutor;
 
-CREATE FUNCTION cria_instrutor_false() RETURNS instrutor AS $$
+CREATE FUNCTION cria_instrutor_falso() RETURNS instrutor AS $$
 	SELECT 22 AS id, 'Nome falso' AS nome, 200.0 AS salario
 $$ LANGUAGE SQL;
 
-SELECT cria_instrutor_false();
+SELECT cria_instrutor_falso();
 
 SELECT * FROM cria_instrutor_false();
 
@@ -31,4 +31,47 @@ $$ LANGUAGE SQL;
 
 SELECT * FROM instrutores_bem_pagos(300);
 
+CREATE TABLE a (nome VARCHAR(255) NOT NULL);
 
+CREATE OR REPLACE FUNCTION cria_a(nome VARCHAR) RETURNS void AS $$
+	BEGIN
+		INSERT INTO a (nome) VALUES('Patricia');
+	END;
+$$ LANGUAGE plpgsql;
+
+SELECT cria_a('Vinicius Dias');
+
+SELECT * FROM a;
+
+CREATE OR REPLACE FUNCTION cria_instrutor_falso() RETURNS instrutor AS $$
+	BEGIN
+		RETURN ROW(22, 'Nome falso', 200.0)::instrutor; 
+	END;
+$$ LANGUAGE plpgsql;
+
+SELECT cria_instrutor_falso();
+
+SELECT * FROM cria_instrutor_false();
+
+
+CREATE OR REPLACE FUNCTION cria_instrutor_falso2() RETURNS instrutor AS $$
+	DECLARE
+		retorno instrutor;
+	BEGIN
+		SELECT 22, 'Nome falso', 200.0 INTO retorno; 
+		RETURN retorno;
+	END;
+$$ LANGUAGE plpgsql;
+
+SELECT cria_instrutor_falso2();
+
+SELECT id, salario FROM cria_instrutor_falso2();
+
+
+CREATE OR REPLACE FUNCTION instrutores_bem_pagos2(valor_salario DECIMAL) RETURNS SETOF instrutor AS $$
+	BEGIN
+		RETURN QUERY SELECT * FROM instrutor WHERE salario > valor_salario;
+	END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM instrutores_bem_pagos2(300);
